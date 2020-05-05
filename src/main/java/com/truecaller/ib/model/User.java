ec88239bb12d4c66af671c1adfb308dc0d18b7c3
Gaurav Atul <asperanza23@gmail.com>
2020-05-05 09:50:06 +0530

    spring security implemented

--- src/main/java/com/truecaller/ib/model/User.java
+++ src/main/java/com/truecaller/ib/model/User.java
@@ -12,4 +12,51 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String username;
+
+    private String password;
+
+    private boolean active;
+
+    private String roles;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
 }
---
+++ src/main/java/com/truecaller/ib/security/CustomUserDetailService.java
@@ -0,0 +1,26 @@
+package com.truecaller.ib.security;
+
+import com.truecaller.ib.model.User;
+import com.truecaller.ib.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUsername(username);
+        user.orElseThrow(() -> new UsernameNotFoundException("User Not Found."));
+
+        return user.map(CustomUserDetail::new).get();
+    }
+}
